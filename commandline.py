c4ff15e7f4ec62411c74ed0cb7ae055d99585157
Melwin P Sebastian <melwinpsebastian@Melwins-Laptop.hitronhub.home>
2024-05-13 08:07:32 -0600

    made desicion function

--- commandline.py
+++ commandline.py
@@ -13,7 +13,25 @@ def machine_input():
   machine = random.randint(0, 2)
   return machine
 
+"""
+Purpose: To make decision on the player whether he wins, loses or tie"
+Returns:  Tied - when there is a tie
+          Won - when player wins
+          Lost - when player loses
+"""
 def decision(player, machine):
+  if player == machine:
+    return "Tied"
+
+  elif player == 0 and machine ==2:
+    return "Won"
+
+  elif player == 1 and machine == 0:
+    return "Won"
+
+  else:
+    return "Lost"
+
 
 
 
