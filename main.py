8f73058e5793a91c84b9137306fe8b6b3c72a163
Melwin P Sebastian <melwinpsebastian@Melwins-Laptop.hitronhub.home>
2024-05-17 07:23:24 -0600

    added main file psuedocode

---
+++ main.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class rock_paper_scissors(Enum):
+    rock = 0
+    paper = 1
+    scissors = 2
+
+play = True
+while play:
+    print("Welcome to arcade world")
+    player = int(input("Which game would you like to"))
+    while(player == 1 or player == 2):
+        if(player == 1):
+            #call rock paper
+            pass
+            #do u want to play again
+
+
+        else:
+            #number guessing game
+            pass
+            #do u want to play again
+
