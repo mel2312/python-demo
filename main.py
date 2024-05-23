4cc10e3ee69a0dfbeee08d9091372c62d8432714
Melwin P Sebastian <melwinperumpral@gmail.com>
2024-05-23 06:22:15 -0600

    added print statement except number guessing terminal to be implemented

--- main.py
+++ main.py
@@ -10,14 +10,28 @@ class rock_paper_scissors(Enum):
 play = True
 while play:
     print("Welcome to arcade world")
-    player = int(input("Which game would you like to"))
+    print("these are the options:\n 1)Rock Paper Scissors\n 2)Number Guessing")
+    player = int(input("Which game would you like to play?"))
     play_again = "y"
-    while((player == 1 or player == 2) and play_again == "y"):
-        if(player == 1):
-            print("Rock")
+    while (player == 1 or player == 2) and play_again == "y":
+        if player == 1:
+            print("Lets play Rock Paper Scissors!")
+            print("what do you choose? rock = 0, paper = 1 or scissors = 2: ")
             player_input = rpk.player_input()
             machine_input = rpk.machine_input()
-            play_again = input("Do you want to play again? y or n")
+            result = rpk.decision(player_input, machine_input)
+            print(f"Computer chose {str(rock_paper_scissors(machine_input)).replace("rock_paper_scissors.", '')} and you chose {str(rock_paper_scissors(player_input)).replace("rock_paper_scissors.", '')}")
+
+            if result == "Tied":
+                print("You tied")
+
+            elif result == "Won":
+                print("Player won")
+
+            else:
+                print("Computer won")
+
+            play_again = input("Do you want to play again? y or n: ")
 
 
         else:
@@ -26,3 +40,5 @@ while play:
             ng.machine_input()
             play_again = input("Do you want to play again? y or n")
 
+    print("Thanks for playing!")
+    print("Do you want to play again in the arcade? y or n")
